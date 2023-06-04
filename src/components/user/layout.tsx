9d625e3365fdb4b1957acc646b078e30a62b28fc
Haneen Alrewaished <haneenalrewaished@Haneens-MacBook-Pro.local>
2023-06-04 13:12:17 +0300

    edit: language

--- src/components/user/layout.tsx
+++ src/components/user/layout.tsx
@@ -7,13 +7,13 @@ interface LayoutProps {
     children: ReactElement;
 }
 function Layout({ children }: LayoutProps) {
-    const { i18n } = useTranslation();
-    useEffect(() => {
-        i18n.changeLanguage(localStorage.getItem("language") || "en");
-    }, [i18n]);
+    // const { i18n } = useTranslation();
+    // useEffect(() => {
+    //     i18n.changeLanguage(localStorage.getItem("language") || "en");
+    // }, [i18n]);
 
     return (
-        <div dir={i18n.language === "en" ? "ltr" : "rtl"} className=" bg-homeBackground bg-cover bg-no-repeat w-full h-screen">
+        <div dir={"ltr"} className=" bg-homeBackground bg-cover bg-no-repeat w-full h-screen">
             <NavBar />
             {children}
             <Footer />
